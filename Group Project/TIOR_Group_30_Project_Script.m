1002aa0b671404aeb3c70fcd9ef4fcac69ae7ca8
Swanbabe <swachris@gmail.com>
2021-05-23 17:06:45 +1000

    incl poisson distr
    Co-Authored-By: brienomark <83943342+brienomark@users.noreply.github.com>

--- Group Project/TIOR_Group_30_Project_Script.m
+++ Group Project/TIOR_Group_30_Project_Script.m
@@ -2,11 +2,11 @@
 % MEMBERS: Chris Swan, Dominic Yew, Greta Di Lorenzo, Mark O'Brien
 
 
-numSensors = 300;   % Number of sensors we want to test with
+numSensors = 200;   % Number of sensors we want to test with
 
-fieldSize = 5000;     % Dimensions of the size of the field we are dealing with for uniformly distributed sensors 
+fieldSize = 100;     % Dimensions of the size of the field we are dealing with for uniformly distributed sensors 
                     % *ASSUMING SQUARE FIELD*
-%lambda = 50;         % Parameter for Poisson distribution of sensors     
+lambda = 500;         % Parameter for Poisson distribution of sensors     
 
 init_step = 0.1;     % Initial step size
 epsilon = 0.000001;   % Algorithm termination threshold
@@ -16,26 +16,29 @@ X_Poisson = [];      % Matrix containing all (x,y) coordinates of our sensors (F
 
 for i = 1:numSensors 
     % random generation of sensor coordinates following Uniform distribution 
-    x_unif = [random('Uniform', -fieldSize, fieldSize), random('Uniform', -fieldSize, fieldSize)];
-    X_Uniform = [X_Uniform; x_unif]
+    %x_unif = [random('Uniform', -fieldSize, fieldSize), random('Uniform', -fieldSize, fieldSize)];
+    %X_Uniform = [X_Uniform; x_unif]
     
     % random generation of sensor coordinates following Poisson distribution
-    %x_pois = [random('Poisson', lambda), random('Poisson', lambda)]    
-    %X_Poisson = [X_Poisson; x_pois]
+    x_pois = [random('Poisson', lambda), random('Poisson', lambda)]    
+    X_Poisson = [X_Poisson; x_pois]
 end
 
 S = [random('Uniform', -fieldSize, fieldSize), random('Uniform', -fieldSize, fieldSize)]
 % randomly generate initial relay location
 
-[s_list, norms] = Descent(S, X_Uniform, epsilon);
+%[s_list, norms] = Descent(S, X_Uniform, epsilon);
+[s_list, norms] = Descent(S, X_Poisson, epsilon);
 disp(s_list);
 disp(norms);
 
-X_Uniform_x1 = X_Uniform(:,1);
-X_Uniform_x2 = X_Uniform(:,2);
+%X_Uniform_x1 = X_Uniform(:,1);
+%X_Uniform_x2 = X_Uniform(:,2);
+X_Poisson_x1 = X_Poisson(:,1);
+X_Poisson_x2 = X_Poisson(:,2);
 s_list_x1 = s_list(:,1);
 s_list_y1 = s_list(:,2);
-plot(X_Uniform_x1, X_Uniform_x2, ".")
+plot(X_Poisson_x1, X_Poisson_x2, ".")
 hold on
 plot(s_list_x1, s_list_y1, "--xg");
 hold off
